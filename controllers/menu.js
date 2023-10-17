f6fb492d1684010cd53d0fb8cafa66af0f6ae563
totti1019 <colafabi@hotmail.it>
2023-10-17 08:56:50 +0200

    Update menu.js

--- controllers/menu.js
+++ controllers/menu.js
@@ -2,13 +2,14 @@ const mysql = require("mysql2/promise");
 const { Menu } = require("../models/menu");
 
 const getAllMenu = async (req, res) => {
+  let connection = null; // Inizializza la variabile connection a null
   try {
-    // Configura la connessione al tuo database
-    const connection = await mysql.createConnection({
-      host: process.env.HOST_FABIO,
-      user: process.env.USER_FABIO,
-      password: "",
-      database: process.env.DATABASE_FABIO,
+    // Configura la connessione al tuo database su Altervista
+    connection = await mysql.createConnection({
+      host: process.env.HOST_PROLOCO,
+      user: process.env.USER_PROLOCO,
+      password: process.env.PASSWORD_PROLOCO,
+      database: process.env.DATABASE_PROLOCO,
     });
 
     // Esegui una query di selezione
@@ -23,11 +24,22 @@ const getAllMenu = async (req, res) => {
 
     res.status(200).json(results);
   } catch (error) {
-    console.error("Errore durante l'esecuzione della query: " + error.message);
+    console.error("Errore durante l'esecuzione della query:", error);
+
     res.status(500).json({
       code: res.statusCode,
-      message: "Errore durante l'esecuzione della query: " + error.message,
+      message: "Errore durante l'esecuzione della query",
+      error: error.message, // Includi i dettagli dell'errore nella risposta
     });
+  } finally {
+    // Gestisci la chiusura della connessione in caso di errore
+    if (connection) {
+      try {
+        await connection.end();
+      } catch (err) {
+        console.error("Errore durante la chiusura della connessione:", err);
+      }
+    }
   }
 };
 
