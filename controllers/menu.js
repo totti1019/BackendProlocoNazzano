33e8073e156556f89ff6ea523da82edf6d273f61
totti1019 <colafabi@hotmail.it>
2023-10-17 07:46:42 +0200

    Update menu.js

--- controllers/menu.js
+++ controllers/menu.js
@@ -1,52 +1,33 @@
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 const { Menu } = require("../models/menu");
 
 const getAllMenu = async (req, res) => {
-  // Configura la connessione al tuo database
-  const connection = mysql.createConnection({
-    host: process.env.HOST_FABIO,
-    user: process.env.USER_FABIO,
-    password: "",
-    database: process.env.DATABASE_FABIO,
-  });
-
   try {
-    // Connessione al database
-    connection.connect((err) => {
-      if (err) {
-        console.error("Errore di connessione al database: " + err);
-        return res.status(404).json({
-          code: res.statusCode,
-          message: "Errore di connessione al database: " + err.stack,
-        });
-      }
-      console.log("Connessione al database riuscita.");
-
-      // Esegui una query di selezione
-      const sqlQuery = `SELECT * FROM ${process.env.TABLE_NAME_MENU}`;
-      connection.query(sqlQuery, (err, results) => {
-        if (err) {
-          console.error(
-            "Errore durante l'esecuzione della query: " + err.message
-          );
-
-          return res.status(404).json({
-            code: res.statusCode,
-            message: "Errore durante l'esecuzione della query: " + err.message,
-          });
-        }
-
-        // I risultati della query sono nell'array "results"
-        console.log("Risultati della query:", results);
-        // Chiudi la connessione al database quando hai finito
-        connection.end();
-      });
+    // Configura la connessione al tuo database
+    const connection = await mysql.createConnection({
+      host: process.env.HOST_PROLOCO,
+      user: process.env.USER_PROLOCO,
+      password: process.env.PASSWORD_PROLOCO,
+      database: process.env.DATABASE_PROLOCO,
     });
 
-    //const menu = await Menu.find();
-    //res.status(200).json(menu);
+    // Esegui una query di selezione
+    const [results, fields] = await connection.execute(
+      `SELECT * FROM ${process.env.TABLE_NAME_MENU}`
+    );
+
+    // Chiudi la connessione al database quando hai finito
+    await connection.end();
+
+    console.log("Risultati della query:", results);
+
+    res.status(200).json(results);
   } catch (error) {
-    res.status(404).json({ code: res.statusCode, message: error.message });
+    console.error("Errore durante l'esecuzione della query: " + error.message);
+    res.status(500).json({
+      code: res.statusCode,
+      message: "Errore durante l'esecuzione della query: " + error.message,
+    });
   }
 };
 
