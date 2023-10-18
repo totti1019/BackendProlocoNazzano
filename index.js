5a40428bacdc7704a2bb07cb86be287173ad4c84
totti1019 <colafabi@hotmail.it>
2023-10-18 19:28:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,26 @@ const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
+// Middleware per abilitare CORS
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://fabiocola.altervista.org/"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  res.setHeader("Access-Control-Max-Age", 7200);
+  next();
+});
+
 app.use(express.json());
 
 app.use("/users", authenticateToken, usersRouters);
