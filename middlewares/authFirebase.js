c7837d400e70c6a4ae2f8590557d6376551d1855
totti1019 <colafabi@hotmail.it>
2023-10-28 19:23:26 +0200

    Update authFirebase.js

--- middlewares/authFirebase.js
+++ middlewares/authFirebase.js
@@ -2,7 +2,9 @@ const { getAuth, signInWithCustomToken } = require("firebase/auth");
 
 // Middleware per verificare l'autenticazione prima di consentire una chiamata API
 const requireAuthFirebase = async (req, res, next) => {
+  console.error(req.headers);
   const authHeader = req.headers["authorization"];
+  console.error(authHeader);
   const token = authHeader && authHeader.split(" ")[1];
 
   const auth = getAuth();
