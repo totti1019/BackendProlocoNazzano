307f0a3910a6f39f08e85cd52fa932a485e97951
totti1019 <colafabi@hotmail.it>
2023-10-29 16:37:31 +0100

    remove cors

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Import delle librerie e moduli necessari
 const express = require("express");
-const cors = require("cors");
 const http = require("http");
 const { Server } = require("socket.io");
 const dotenv = require("dotenv");
@@ -21,7 +20,6 @@ const PORT = process.env.PORT || 3000;
 
 // Configura l'app Express
 const app = express();
-app.use(cors());
 // Impostazione del middleware per il parsing del corpo delle richieste come JSON
 app.use(express.json());
 const httpServer = http.createServer(app);
@@ -34,6 +32,7 @@ const io = new Server(httpServer, {
   },
 });
 
+// Configurazione delle impostazioni CORS con Express
 app.use(function (req, res, next) {
   var oneof = false;
   /*if (req.headers.origin) {
@@ -65,38 +64,6 @@ app.use(function (req, res, next) {
     next();
   }
 });
-
-// Configurazione delle impostazioni CORS con Express
-/*app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://fabiocola.altervista.org"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin"
-  );
-  res.setHeader(
-    "Access-Control-Expose-Headers",
-    "Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  // Aggiungi un nuovo blocco per gestire le richieste OPTIONS
-  if (req.method === "OPTIONS") {
-    // Rispondi alle richieste OPTIONS con i dettagli CORS appropriati
-    res.set("Access-Control-Allow-Origin", "https://fabiocola.altervista.org");
-    res.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-    res.set("Access-Control-Allow-Headers", "Authorization, Content-Type");
-    res.status(200).end();
-  } else {
-    // Altrimenti, gestisci le altre richieste come lo fai attualmente
-    console.log("ALTRO: ", req.method);
-    next();
-  }
-}); */
 
 // Collegamento delle rotte alle relative parti dell'app
 app.use("/users", authenticateToken, usersRouters);
