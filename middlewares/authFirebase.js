81668a1791c6caa6e44fe2f5bfab8511ffb43823
totti1019 <colafabi@hotmail.it>
2023-10-29 08:44:02 +0100

    Update authFirebase.js

--- middlewares/authFirebase.js
+++ middlewares/authFirebase.js
@@ -43,24 +43,43 @@ const { getAuth, signInWithCustomToken } = require("firebase/auth");
 const requireAuthFirebase = async (req, res, next) => {
   try {
     // Verifica se è una richiesta POST e se l'header "Authorization" è presente
-    console.error("DOBYYYYYYYYYYY", req.body);
-    console.error("HEADER", req.method);
     if (req.method === "POST") {
-      console.error(req.headers);
-      //const token = req.headers.authorization;
-      //console.log("Token:", token);
-      // Continua con la verifica del token o qualsiasi altra logica necessaria
+      const authHeader = req.headers["authorization"];
+      const token = authHeader && authHeader.split(" ")[1];
+
+      const auth = getAuth();
+
+      await signInWithCustomToken(auth, token)
+        .then((user) => {
+          if (user) {
+            next();
+          } else {
+            // L'utente non è autenticato, restituisci un errore o reindirizza all'accesso
+            res.status(401).json({
+              code: res.statusCode,
+              esito: false,
+              message: "Utente non autenticato",
+            });
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          res.status(401).json({
+            code: res.statusCode,
+            esito: false,
+            message: "Utente non autenticato",
+          });
+        });
       next();
-    } else {
-      // Gestisci il caso in cui l'header "Authorization" non sia presente
-      // Puoi inviare una risposta di errore o eseguire altre azioni necessarie
     }
     // Continua con il middleware successivo o la gestione della richiesta
   } catch (error) {
-    // Gestisci eventuali errori
-    console.error(error);
-    // Invia una risposta di errore se necessario
-    res.status(500).json({ error: "Internal server error" });
+    console.log(error);
+    res.status(401).json({
+      code: res.statusCode,
+      esito: false,
+      message: "Utente non autenticato",
+    });
   }
 };
 
