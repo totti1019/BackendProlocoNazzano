809ea3fa8215f033007517a7253a6fd395b6409a
totti1019 <colafabi@hotmail.it>
2023-10-29 07:50:52 +0100

    Update authFirebase.js

--- middlewares/authFirebase.js
+++ middlewares/authFirebase.js
@@ -1,16 +1,16 @@
 const { getAuth, signInWithCustomToken } = require("firebase/auth");
 
 // Middleware per verificare l'autenticazione prima di consentire una chiamata API
-const requireAuthFirebase = async (req, res, next) => {
+/*const requireAuthFirebase = async (req, res, next) => {
   try {
-    console.error("CERCO IL TOKEN ", req.headers);
-    //const authHeader = req.headers["authorization"];
-    // console.error(authHeader);
-    // const token = authHeader && authHeader.split(" ")[1];
+    console.error("CERCO IL ROKEN ", req.headers.authorization);
+    const authHeader = req.headers["authorization"];
+    console.error(authHeader);
+    const token = authHeader && authHeader.split(" ")[1];
 
-    // const auth = getAuth();
+    const auth = getAuth();
 
-    /* await signInWithCustomToken(auth, token)
+    await signInWithCustomToken(auth, token)
       .then((user) => {
         if (user) {
           next();
@@ -30,7 +30,7 @@ const requireAuthFirebase = async (req, res, next) => {
           esito: false,
           message: "Utente non autenticato",
         });
-      }); */
+      });
   } catch (error) {
     console.log(error);
     res.status(401).json({
@@ -38,6 +38,27 @@ const requireAuthFirebase = async (req, res, next) => {
       esito: false,
       message: "Utente non autenticato",
     });
+  }
+}; */
+const requireAuthFirebase = async (req, res, next) => {
+  try {
+    // Verifica se è una richiesta POST e se l'header "Authorization" è presente
+    if (req.method === "POST" && req.headers.authorization) {
+      console.error(req.headers);
+      const token = req.headers.authorization;
+      console.log("Token:", token);
+      // Continua con la verifica del token o qualsiasi altra logica necessaria
+    } else {
+      // Gestisci il caso in cui l'header "Authorization" non sia presente
+      // Puoi inviare una risposta di errore o eseguire altre azioni necessarie
+    }
+    // Continua con il middleware successivo o la gestione della richiesta
+    next();
+  } catch (error) {
+    // Gestisci eventuali errori
+    console.error(error);
+    // Invia una risposta di errore se necessario
+    res.status(500).json({ error: "Internal server error" });
   }
 };
 
