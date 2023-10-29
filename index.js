edd0b4f6e0d20db9e354b36ad9e11bc680440982
totti1019 <colafabi@hotmail.it>
2023-10-29 08:37:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -34,6 +34,38 @@ const io = new Server(httpServer, {
   },
 });
 
+app.use(function (req, res, next) {
+  var oneof = false;
+  if (req.headers.origin) {
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    oneof = true;
+  }
+  if (req.headers["access-control-request-method"]) {
+    res.header(
+      "Access-Control-Allow-Methods",
+      req.headers["access-control-request-method"]
+    );
+    oneof = true;
+  }
+  if (req.headers["access-control-request-headers"]) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      req.headers["access-control-request-headers"]
+    );
+    oneof = true;
+  }
+  if (oneof) {
+    res.header("Access-Control-Max-Age", 60 * 60 * 24 * 365);
+  }
+
+  // intercept OPTIONS method
+  if (oneof && req.method == "OPTIONS") {
+    res.send(200);
+  } else {
+    next();
+  }
+});
+
 // Configurazione delle impostazioni CORS con Express
 /*app.use((req, res, next) => {
   res.setHeader(
