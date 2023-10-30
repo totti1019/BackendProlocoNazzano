183c82d00b19374ce7b93f59afdad7613aefe6f6
totti1019 <colafabi@hotmail.it>
2023-10-30 21:18:08 +0100

    fix

--- middlewares/authFirebase.js
+++ middlewares/authFirebase.js
@@ -49,6 +49,60 @@ const requireAuthFirebase = async (req, res, next) => {
   }
 };
 
+const validationToken = async (req, res, next) => {
+  try {
+    if (req.method !== "POST") {
+      return next();
+    }
+
+    const authHeader = req.headers["authorization"];
+
+    if (!authHeader) {
+      return res.status(401).json({
+        code: 401,
+        esito: false,
+        message: "Token di autorizzazione mancante",
+      });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({
+        code: 401,
+        esito: false,
+        message: "Token di autorizzazione mancante",
+      });
+    }
+
+    const auth = getAuth();
+
+    try {
+      await signInWithCustomToken(auth, token);
+      return res.status(200).json({
+        code: 200,
+        esito: true,
+        message: "Token valido",
+      });
+    } catch (error) {
+      console.error(error);
+      return res.status(401).json({
+        code: 401,
+        esito: false,
+        message: "Token non valido",
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      code: 500,
+      esito: false,
+      message: "Errore durante l'autenticazione",
+    });
+  }
+};
+
 module.exports = {
   requireAuthFirebase,
+  validationToken,
 };
--- routers/auth.js
+++ routers/auth.js
@@ -5,6 +5,9 @@ const {
   login,
   loginAnonymous,
 } = require("../controllers/auth");
+
+const { validationToken } = require("../middlewares/authFirebase");
+
 const validateFields = require("../middlewares/validateFields");
 
 const router = express.Router();
@@ -13,5 +16,6 @@ router.post("/register", validateFields, register);
 router.post("/loginGoogle", validateFields, loginGoogle);
 router.post("/login", validateFields, login);
 router.post("/", validateFields, loginAnonymous);
+router.post("/validationToken", validateFields, validationToken);
 
 module.exports = router;
