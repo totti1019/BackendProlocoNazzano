8eb0b8b5ac454d6f2a2d07c6e4468ea5cbab858c
totti1019 <colafabi@hotmail.it>
2023-11-07 16:08:13 +0100

    Update utils.js

--- controllers/utils/utils.js
+++ controllers/utils/utils.js
@@ -23,7 +23,7 @@ const database = getDatabase(app);
 
 // Percorso assoluto del file
 const filePath = path.join(__dirname, "sharedData.json");
-
+console.error("PERCORSO DB filePath ", filePath);
 // Funzione per il salvataggio dei dati in un file
 function saveSharedData(data) {
   const dataToSave = JSON.stringify(data);
@@ -32,7 +32,7 @@ function saveSharedData(data) {
 
 function loadSharedData() {
   const loadedData = fs.readFileSync(filePath, "utf8");
-  console.error("PERCORSO DB", loadedData);
+  console.error("PERCORSO DB ", loadedData);
   return JSON.parse(loadedData);
 }
 
